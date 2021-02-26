c53d0c73a9a3258a678909d3cea18d8c2283fd21
Csabuda Nóra <79472066+csnorka@users.noreply.github.com>
2021-02-26 12:37:30 +0100

    akasztófa

--- Akasztófa.Designer.cs
+++ Akasztófa.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Akasztófa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Akasztófa";
             this.Text = "Akasztófa";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
