493403c82ccd72f9a6e593e5a23b882e78002da6
Csabuda Nóra <79472066+csnorka@users.noreply.github.com>
2021-02-28 13:40:42 +0100

    akasztófa-majdnem

--- Akasztófa.Designer.cs
+++ Akasztófa.Designer.cs
@@ -28,7 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flp_word = new System.Windows.Forms.FlowLayoutPanel();
+            this.gb_kitalalndo = new System.Windows.Forms.GroupBox();
+            this.flp_buttons = new System.Windows.Forms.FlowLayoutPanel();
+            this.l_szohossz = new System.Windows.Forms.Label();
+            this.p_hangman = new System.Windows.Forms.Panel();
+            this.l_hibalehetoseg = new System.Windows.Forms.Label();
+            this.l_info = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_rosszvalaszok = new System.Windows.Forms.TextBox();
+            this.l_proba = new System.Windows.Forms.Label();
+            this.b_ujjatek = new System.Windows.Forms.Button();
+            this.b_kilepes = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // flp_word
+            // 
+            this.flp_word.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flp_word.Location = new System.Drawing.Point(12, 39);
+            this.flp_word.Name = "flp_word";
+            this.flp_word.Size = new System.Drawing.Size(509, 126);
+            this.flp_word.TabIndex = 0;
+            // 
+            // gb_kitalalndo
+            // 
+            this.gb_kitalalndo.Location = new System.Drawing.Point(12, 39);
+            this.gb_kitalalndo.Name = "gb_kitalalndo";
+            this.gb_kitalalndo.Size = new System.Drawing.Size(509, 100);
+            this.gb_kitalalndo.TabIndex = 0;
+            this.gb_kitalalndo.TabStop = false;
+            this.gb_kitalalndo.Text = "A kitalálandó szó";
+            // 
+            // flp_buttons
+            // 
+            this.flp_buttons.Location = new System.Drawing.Point(12, 246);
+            this.flp_buttons.Name = "flp_buttons";
+            this.flp_buttons.Size = new System.Drawing.Size(509, 133);
+            this.flp_buttons.TabIndex = 1;
+            // 
+            // l_szohossz
+            // 
+            this.l_szohossz.AutoSize = true;
+            this.l_szohossz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_szohossz.Location = new System.Drawing.Point(12, 168);
+            this.l_szohossz.Name = "l_szohossz";
+            this.l_szohossz.Size = new System.Drawing.Size(46, 18);
+            this.l_szohossz.TabIndex = 0;
+            this.l_szohossz.Text = "label1";
+            // 
+            // p_hangman
+            // 
+            this.p_hangman.Location = new System.Drawing.Point(557, 39);
+            this.p_hangman.Name = "p_hangman";
+            this.p_hangman.Size = new System.Drawing.Size(219, 340);
+            this.p_hangman.TabIndex = 2;
+            // 
+            // l_hibalehetoseg
+            // 
+            this.l_hibalehetoseg.AutoSize = true;
+            this.l_hibalehetoseg.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_hibalehetoseg.Location = new System.Drawing.Point(12, 186);
+            this.l_hibalehetoseg.Name = "l_hibalehetoseg";
+            this.l_hibalehetoseg.Size = new System.Drawing.Size(46, 18);
+            this.l_hibalehetoseg.TabIndex = 3;
+            this.l_hibalehetoseg.Text = "label1";
+            // 
+            // l_info
+            // 
+            this.l_info.AutoSize = true;
+            this.l_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_info.Location = new System.Drawing.Point(12, 225);
+            this.l_info.Name = "l_info";
+            this.l_info.Size = new System.Drawing.Size(46, 18);
+            this.l_info.TabIndex = 4;
+            this.l_info.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(271, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Rossz válaszok:";
+            // 
+            // tb_rosszvalaszok
+            // 
+            this.tb_rosszvalaszok.Enabled = false;
+            this.tb_rosszvalaszok.Location = new System.Drawing.Point(396, 166);
+            this.tb_rosszvalaszok.Multiline = true;
+            this.tb_rosszvalaszok.Name = "tb_rosszvalaszok";
+            this.tb_rosszvalaszok.Size = new System.Drawing.Size(125, 38);
+            this.tb_rosszvalaszok.TabIndex = 6;
+            // 
+            // l_proba
+            // 
+            this.l_proba.AutoSize = true;
+            this.l_proba.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.l_proba.Location = new System.Drawing.Point(475, 382);
+            this.l_proba.Name = "l_proba";
+            this.l_proba.Size = new System.Drawing.Size(46, 18);
+            this.l_proba.TabIndex = 7;
+            this.l_proba.Text = "label1";
+            // 
+            // b_ujjatek
+            // 
+            this.b_ujjatek.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b_ujjatek.Location = new System.Drawing.Point(557, 407);
+            this.b_ujjatek.Name = "b_ujjatek";
+            this.b_ujjatek.Size = new System.Drawing.Size(89, 31);
+            this.b_ujjatek.TabIndex = 8;
+            this.b_ujjatek.Text = "Új játék";
+            this.b_ujjatek.UseVisualStyleBackColor = true;
+            this.b_ujjatek.Click += new System.EventHandler(this.b_ujjatek_Click);
+            // 
+            // b_kilepes
+            // 
+            this.b_kilepes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b_kilepes.Location = new System.Drawing.Point(687, 407);
+            this.b_kilepes.Name = "b_kilepes";
+            this.b_kilepes.Size = new System.Drawing.Size(89, 31);
+            this.b_kilepes.TabIndex = 9;
+            this.b_kilepes.Text = "Kilépés";
+            this.b_kilepes.UseVisualStyleBackColor = true;
+            this.b_kilepes.Click += new System.EventHandler(this.b_kilepes_Click);
             // 
             // Akasztófa
             // 
@@ -37,12 +161,38 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.b_kilepes);
+            this.Controls.Add(this.b_ujjatek);
+            this.Controls.Add(this.l_proba);
+            this.Controls.Add(this.tb_rosszvalaszok);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.l_info);
+            this.Controls.Add(this.l_hibalehetoseg);
+            this.Controls.Add(this.p_hangman);
+            this.Controls.Add(this.l_szohossz);
+            this.Controls.Add(this.flp_buttons);
+            this.Controls.Add(this.gb_kitalalndo);
+            this.Controls.Add(this.flp_word);
             this.Name = "Akasztófa";
-            this.Text = "Akasztófa";
+            this.Text = "fl";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flp_word;
+        private System.Windows.Forms.GroupBox gb_kitalalndo;
+        private System.Windows.Forms.FlowLayoutPanel flp_buttons;
+        private System.Windows.Forms.Label l_szohossz;
+        private System.Windows.Forms.Panel p_hangman;
+        private System.Windows.Forms.Label l_hibalehetoseg;
+        private System.Windows.Forms.Label l_info;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_rosszvalaszok;
+        private System.Windows.Forms.Label l_proba;
+        private System.Windows.Forms.Button b_ujjatek;
+        private System.Windows.Forms.Button b_kilepes;
     }
 }
--- Akasztófa.cs
+++ Akasztófa.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace logikai_jatekok
 {
@@ -15,6 +16,211 @@ namespace logikai_jatekok
         public Akasztófa()
         {
             InitializeComponent();
+            Beolvasás();
+            SzóKiválaszt();
+            GombHozzáad();
+            LabelsHozzáad();
+        }
+        public string[] szavak;
+        public string[] alapbetuk = {"A","Á","B","C","D","E","É","F","G","H","I","Í","J","K","L","M","N","O",
+            "Ó","Ö","Ő","P","Q","R","S","T","U","Ú","Ü","Ű","V","W","X","Y","Z"};
+        public string aktszo;
+        public List<Label> labels = new List<Label>();
+        public Random random = new Random();
+        public enum HangState
+        {
+            Üres, Alap, FüggOszlop, VizOszlop, Kötél, Fej, Test, BalKar, BalKéz, JobbKar, JobbKéz, BalLáb, Jobbláb
+        }
+        public HangState AktHangState = HangState.Üres;
+        public HangState HangStateSize = HangState.Jobbláb;
+        public void Beolvasás()
+        {
+            string[] sorok = File.ReadAllLines("szavak.txt");
+            szavak = new string[sorok.Length];
+            for (int i = 0; i < szavak.Length; i++)
+            {
+                szavak[i] = sorok[i].ToUpper();
+            }
+        }
+        private void SzóKiválaszt()
+        {
+            int r = random.Next(0, szavak.Length);
+            aktszo = szavak[r];
+        }
+        private void GombHozzáad()
+        {
+            for (int i = 0; i < alapbetuk.Length; i++)
+            {
+                Button b = new Button();
+                b.Text = alapbetuk[i];
+                b.Parent = flp_buttons;
+                b.Font = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold);
+                b.Size = new Size(30, 30);
+                b.BackColor = Color.Lavender;
+                b.Click += B_Click;
+            }
+        }
+        private void LabelsHozzáad()
+        {
+            gb_kitalalndo.Controls.Clear();
+            labels.Clear();
+            char[] letters = aktszo.ToCharArray();
+            int hossz = letters.Length;
+            int hely = gb_kitalalndo.Width / hossz;
+            for (int i = 0; i < hossz; i++)
+            {
+                Label l = new Label();
+                l.Text = " __ ";
+                l.Location = new Point(10 + i * hely, gb_kitalalndo.Height - 30);
+                l.Parent = gb_kitalalndo;
+                l.BringToFront();
+                labels.Add(l);
+            }
+            l_szohossz.Text = "A szó hossza: " + Convert.ToString(hossz);
+            l_hibalehetoseg.Text = "Hibalehetőség: " + Convert.ToInt32(HangStateSize);
+        }
+        private void B_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            char betuclicked = b.Text.ToCharArray()[0];
+            b.Enabled = false;
+
+            if ((aktszo = aktszo.ToUpper()).Contains(betuclicked))
+            {
+                l_info.Text = "Fantasztikus!";
+                l_info.ForeColor = Color.DarkGreen;
+                char[] betuk = aktszo.ToCharArray();
+                for (int i = 0; i < aktszo.Length; i++)
+                {
+                    if (betuk[i] == betuclicked)
+                    {
+                        labels[i].Text = betuclicked.ToString();
+                        labels[i].Font = new Font(FontFamily.GenericSansSerif, 14);
+                    }
+                }
+                if (labels.Where(x => x.Text.Equals(" __ ")).Any())
+                {
+                    return;
+                }
+                l_info.Text = "Gratulálok! Nyertél! :)";
+                l_info.ForeColor = Color.DarkGreen;
+                flp_buttons.Enabled = false;
+                flp_word.Enabled = false;
+            }
+            else
+            {
+                l_info.Text = "Nem jó!";
+                l_info.ForeColor = Color.DarkRed;
+                if (AktHangState != HangState.Jobbláb)
+                {
+                    AktHangState++;
+                }
+                l_hibalehetoseg.Text = "Hibalehetőség: " + Convert.ToInt32(HangStateSize - AktHangState);
+                tb_rosszvalaszok.Text += betuclicked.ToString() + ", ";
+                if (AktHangState == HangState.Jobbláb)
+                {
+                    l_info.Text = "Vesztettél!";
+                    l_info.ForeColor = Color.DarkRed;
+                    flp_buttons.Enabled = false;
+                    flp_word.Enabled = false;
+
+                    for (int i = 0; i < aktszo.Length; i++)
+                    {
+                        if (labels[i].Text.Equals(" __ "))
+                        {
+                            labels[i].Text = aktszo[i].ToString();
+                            labels[i].ForeColor = Color.DarkBlue;
+                            labels[i].Font = new Font(FontFamily.GenericSansSerif, 14);
+                        }
+                    }
+                }
+            }
+        }
+        private void p_hangman_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.DarkBlue, 5);
+            Graphics g = e.Graphics;
+            if (AktHangState >= HangState.Alap)
+            {
+                //Alap
+                g.DrawLine(p, 250, 300, 50, 300);
+            }
+            if (AktHangState >= HangState.FüggOszlop)
+            {
+                //Függőleges oszlop
+                g.DrawLine(p, 200, 300, 200, 25);
+            }
+            if (AktHangState >= HangState.VizOszlop)
+            {
+                //Vízszintes oszlop
+                g.DrawLine(p, 205, 25, 80, 25);
+            }
+            if (AktHangState >= HangState.Kötél)
+            {
+                //Kötél
+                g.DrawLine(p, 100, 80, 100, 25);
+            }
+            if (AktHangState >= HangState.Fej)
+            {
+                //Fej
+                Rectangle fej = new Rectangle(82, 80, 35, 35);
+                g.DrawEllipse(p, fej);
+            }
+            if (AktHangState >= HangState.Test)
+            {
+                //Test
+                g.DrawLine(p, 100, 117, 100, 190);
+            }
+            if (AktHangState >= HangState.BalKar)
+            {
+                //Bal kar
+                g.DrawLine(p, 100, 120, 75, 170);
+            }
+            if (AktHangState >= HangState.BalKéz)
+            {
+                //Bal kéz
+                Rectangle bkez = new Rectangle(70, 170, 5, 5);
+                g.DrawEllipse(p, bkez);
+            }
+            if (AktHangState >= HangState.JobbKar)
+            {
+                //Jobb kar
+                g.DrawLine(p, 100, 120, 125, 170);
+            }
+            if (AktHangState >= HangState.JobbKéz)
+            {
+                //Jobb kéz
+                Rectangle jkez = new Rectangle(125, 170, 5, 5);
+                g.DrawEllipse(p, jkez);
+            }
+            if (AktHangState >= HangState.BalLáb)
+            {
+                //Bal láb
+                g.DrawLine(p, 100, 185, 80, 250);
+            }
+            if (AktHangState >= HangState.Jobbláb)
+            {
+                //Jobb láb
+                g.DrawLine(p, 100, 185, 120, 250);
+            }
+            l_proba.Text = "Próba: " + AktHangState;
+        }
+        private void b_ujjatek_Click(object sender, EventArgs e)
+        {
+            flp_buttons.Controls.Clear();
+            l_info.Text = " - ";
+            tb_rosszvalaszok.Clear();
+            flp_buttons.Enabled = true;
+            HangStateSize = HangState.Jobbláb;
+            AktHangState = HangState.Üres;
+            p_hangman.Controls.Clear();
+            SzóKiválaszt();
+            GombHozzáad();
+            LabelsHozzáad();
+        }
+        private void b_kilepes_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
