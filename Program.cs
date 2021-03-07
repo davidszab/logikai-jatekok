42cc8fa25cb2740f85f2369fc5160c06e9afd4f8
Erdosi Balint <79472103+Derto12@users.noreply.github.com>
2021-03-07 19:15:15 +0100

    Uj GameDatabase metodusok

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace logikai_jatekok
     {
         static public string player;
         static public Windows windowIndex = Windows.MainWindow;
-        static public GameDatabase database;
+        static public GameDatabase database = new GameDatabase();
         static public bool runProgram = true;
 
         /// <summary>
@@ -22,42 +22,62 @@ namespace logikai_jatekok
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            do
-            {
-                switch (windowIndex)
-                {
-                    case Windows.MainWindow:
-                        //Application.Run(new MainMenuForm()); 
-                        //MainMenuForm belepesnel Windows.CloseWindows-ra allitja a windowsIndex-et
-                        //ha a felhasznalo valaszt jatekot akkor peddig a valasztott index-re csereli
-                        break;
+            if (!database.ConfigFileExists) database.CreateConfigFile("localhost", "root", "3306", "");
+            database.SetUpDatabase();
 
-                    case Windows.HangmanWindow:
-                        Application.Run(new Akasztófa());
-                        windowIndex = Windows.MainWindow;
-                        break;
+            database.AddPlayer("one");
+            database.AddPlayer("as");
+            database.AddPlayer("da");
 
-                    case Windows.MastermindWindow:
-                        Application.Run(new Mastermind());
-                        windowIndex = Windows.MainWindow;
-                        break;
+            database.SaveData("one", GameTypes.hangman, 21);
+            database.SaveData("da", GameTypes.hangman, 32);
+            database.SaveData("one", GameTypes.mastermind, 55);
+            database.SaveData("da", GameTypes.hangman, 83);
+            database.SaveData("one", GameTypes.hangman, 44);
+            database.SaveData("as", GameTypes.minesweeper, 3);
+            database.SaveData("one", GameTypes.hangman, 94);
+            database.SaveData("one", GameTypes.mastermind, 8);
 
-                    case Windows.MinesweeperWindow:
-                        Application.Run(new MinesweeperForm());
-                        windowIndex = Windows.MainWindow;
-                        break;
+            List<string> name = database.GetPlayers();
 
-                    case Windows.StatisticsWindow:
-                        //Application.Run(new StatisticsForm());
-                        windowIndex = Windows.MainWindow;
-                        break;
+            Console.WriteLine("huhhhh");
 
-                    case Windows.CloseWindows:
-                        runProgram = false;
-                        break;
-                }
+            //do
+            //{
+            //    switch (windowIndex)
+            //    {
+            //        case Windows.MainWindow:
+            //            //Application.Run(new MainMenuForm()); 
+            //            //MainMenuForm belepesnel Windows.CloseWindows-ra allitja a windowsIndex-et
+            //            //ha a felhasznalo valaszt jatekot akkor peddig a valasztott index-re csereli
+            //            break;
 
-            } while (runProgram);
+            //        case Windows.HangmanWindow:
+            //            Application.Run(new Akasztófa());
+            //            windowIndex = Windows.MainWindow;
+            //            break;
+
+            //        case Windows.MastermindWindow:
+            //            Application.Run(new Mastermind());
+            //            windowIndex = Windows.MainWindow;
+            //            break;
+
+            //        case Windows.MinesweeperWindow:
+            //            Application.Run(new MinesweeperForm());
+            //            windowIndex = Windows.MainWindow;
+            //            break;
+
+            //        case Windows.StatisticsWindow:
+            //            //Application.Run(new StatisticsForm());
+            //            windowIndex = Windows.MainWindow;
+            //            break;
+
+            //        case Windows.CloseWindows:
+            //            runProgram = false;
+            //            break;
+            //    }
+
+            //} while (runProgram);
         }
     }
 }
